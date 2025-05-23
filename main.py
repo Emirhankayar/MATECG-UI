07665606791d4331af2fdab649eb3f145a545ecf
Emirhan Kayar <emirhan.kayar80@gmail.com>
2025-05-23 23:16:16 +0200

    eh

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ from PyQt5.QtWidgets import QApplication
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = gui.App()
-    window.showFullScreen()
-    # window.show()
+    # window.showFullScreen()
+    window.show()
     sys.exit(app.exec_())
